fb5292657b50fa9123f04cf803cbc11202bd5ffe
Ufaa <dhuwanee.no1@gmail.com>
2021-08-29 14:26:20 +0900

    20210829

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,6 +13,7 @@ class BookController extends Controller
         $book = new Book();
         $uuid = (string)Str::uuid();
         $book->fill([
+            'id' => 20,
             'uuid' =>  $uuid,
             'name' => 'FillBook',
             'price' => 1500,
@@ -22,6 +23,7 @@ class BookController extends Controller
     public function createBook() {
         $uuid = (string)Str::uuid();
         Book::create([
+            'id' => 20,
             'uuid' =>  $uuid,
             'name' => 'CreateBook',
             'price' => 1200,
@@ -31,9 +33,11 @@ class BookController extends Controller
         $book = new Book();
         $uuid = (string)Str::uuid();
         $book::insert([
+            'id' => 20,
             'uuid' =>  $uuid,
             'name' => 'InsertBook',
             'price' => 1800,
         ]);
     }
 }
+
